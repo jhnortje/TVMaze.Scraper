68136784bf0a7390d5f5edcd79e2a960e37d58b5
jhnortje <jhnortje@gmail.com>
2019-03-08 17:48:43 +0200

    Add files via upload

---
+++ TVMaze.API/Controllers/ShowsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TVMaze.Infrastructure.Data.Repository;
+using TVMaze.Infrastructure.Model;
+
+namespace TVMaze.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShowsController : Controller
+    {
+        private readonly IRepository<ShowModel> _showCastDataRepository;
+        private const int _pageSize = 250;
+
+        public ShowsController(IRepository<ShowModel> showCastDataRepository)
+        {
+            _showCastDataRepository = showCastDataRepository;
+        }
+
+        // http://localhost:59661/api/cast
+        // OR http://localhost:59661/api/cast?page=2
+        [HttpGet]
+        public IActionResult Get(int? page = null)
+        {
+            // Default to first page if parameter is not supplied
+            long rangeToId = ((page ?? 0) + 1) * _pageSize;
+            IEnumerable<ShowModel> showModel = _showCastDataRepository.GetRange((rangeToId - _pageSize) + 1, rangeToId);
+
+            return Ok(showModel);
+        }
+    }
+}
---
+++ TVMaze.API/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TVMaze.Infrastructure.Data.Managers;
+using TVMaze.Infrastructure.Data.Repository;
+using TVMaze.Infrastructure.EntityFramework;
+using TVMaze.Infrastructure.Model;
+
+namespace TVMaze.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationContext>(opts => opts.UseSqlServer(Configuration["ConnectionString:TVMazeDatabase"]));
+            services.AddScoped<IRepository<ShowModel>, ShowCastManager>();            
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
